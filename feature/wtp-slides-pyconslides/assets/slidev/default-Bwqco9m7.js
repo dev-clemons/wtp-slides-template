4798a9047b3296eb8e08f0950e289586ec68ef58
dev-clemons <clemens.s.bachmann@gmail.com>
2026-07-28 15:04:52 +0000

    Deploying to gh-pages from @ dev-clemons/wtp-slides-template@17832ca3787ca9725d73cf75ffa1af76532cc905 🚀

---
+++ feature/wtp-slides-pyconslides/assets/slidev/default-Bwqco9m7.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-CAV2GWiQ.js";import{_ as r}from"../index-DgCVlrKX.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
---
+++ feature/wtp-slides-pyconslides/assets/tigerjython-0N836XZf.js
@@ -0,0 +1,1 @@
+const s="/wtp-slides-template/feature/wtp-slides-pyconslides/assets/jython-DRvmLfH5.png",t="/wtp-slides-template/feature/wtp-slides-pyconslides/assets/tigerjython-C1asWPKp.png";export{s as _,t as a};
