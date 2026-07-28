3c0df27e32815fb6f52b626bb90e3630d0a20f43
dev-clemons <clemens.s.bachmann@gmail.com>
2026-07-28 15:04:46 +0000

    Deploying to gh-pages from @ dev-clemons/wtp-slides-template@b71146b6830ac796a8de5b11d1bc49c1a8aa7954 🚀

---
+++ feature/wtp-slides-doctoral_consortium/assets/slidev/default-DYRNDBcD.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-CILHHp6r.js";import{_ as r}from"../index-BY5MSY6E.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
